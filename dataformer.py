b6ed2375f3352a2a5f7fa8b7d3eceaae38b8cb02
Seva <esp@gctravel.co.jp>
2014-12-25 18:22:16 +0900

    Linescan_

--- dataformer.py
+++ dataformer.py
@@ -1,4 +1,10 @@
 import sqlite3
+import datetime
+print(
+    datetime.datetime.fromtimestamp(
+        int("1284101485")
+    ).strftime('%Y-%m-%d %H:%M:%S')
+)
 db=sqlite3.connect("d:\\temp\\talk.sqlite")
 cur=db.cursor()
 cur.execute("Select ZTEXT from ZMESSAGE where CHAT=3 ORDER BY ZTIMESTAMP ASC")
